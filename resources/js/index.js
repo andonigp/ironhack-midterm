26025b2c4dd9c851f27519bd476f02617f8ae551
Andoni Goicoechea Pederzini <andoni.gope@gmail.com>
2023-01-25 14:24:56 +0100

    Agregado de hamburguer menu.

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,1 +1,10 @@
 /* Crea tu propia lógica para hacer fetch de 3 posts distintos y enseñarlos en la homepage con DOM manipulation */
+
+const hamburguer = document.querySelector("#navbar-toggle")
+const navMenu = document.querySelector("nav ul")
+
+hamburguer.addEventListener("click", () => {
+    hamburguer.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+console.log(navMenu)
