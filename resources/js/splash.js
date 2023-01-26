37e23f1165d89c8efbbabbe3531279b8b2ad1837
Andoni Goicoechea Pederzini <andoni.gope@gmail.com>
2023-01-26 14:31:23 +0100

    About page done. Loading circle donde.

--- resources/js/splash.js
+++ resources/js/splash.js
@@ -1,1 +1,9 @@
 /* Crea tu propia lógica para hacer una splash page que desaparezca */
+// Splash page
+const splash = document.querySelector(".circle");
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  setTimeout(() => {
+    splash.classList.add("display-none");
+  }, 2000);
+});
